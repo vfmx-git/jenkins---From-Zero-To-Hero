76bd8fdb8470ba37c72b863bb372c6a1d29c665a
bulava <bul@ava>
2019-10-09 15:31:34 +0300

    cleaning that all...

---
+++ jenkins-data/jenkins-ansible/crumb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+crumb=$(curl -u "jenkins:1234" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+
+#curl -u "jenkins:1234" -H '$crumb' -X POST  http://jenkins.local:8080/job/ENV/build?delay=0sec
+#curl -u "jenkins:1234" -H '$crumb' -X POST  http://jenkins.local:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup
+
+curl -u "jenkins:1234" -H '$crumb' -X POST  http://jenkins.local:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql--backup1
